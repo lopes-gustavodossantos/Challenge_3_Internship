493f91ba4a6d2348d021e6cee5da0dd2ad76921a
Gustavo dos Santos Lopes <lopes.gustavodossantos@gmail.com>
2023-09-07 17:45:58 -0300

    feat: data.json created for data usage, created card system in the Home Screen

--- ChallengeIII/app/(tabs)/home.tsx
+++ ChallengeIII/app/(tabs)/home.tsx
@@ -1,18 +1,15 @@
 import { useFonts } from "expo-font";
-import {
-  Text,
-  View,
-  Pressable,
-  StyleSheet,
-  Image,
-} from "react-native";
+import { Text, View, Pressable, StyleSheet, Image, Platform } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { FlatList } from "react-native";
+
+import data from "../../assets/api/data.json";
 
 export default function HomeScreen() {
   const navigation = useNavigation();
-
   const [fontsLoaded] = useFonts({
     "Poppins Medium": require("../../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins Regular": require("../../assets/fonts/Poppins-Regular.ttf"),
   });
 
   if (!fontsLoaded) {
@@ -35,6 +32,21 @@ export default function HomeScreen() {
           />
         </Pressable>
       </View>
+
+      <FlatList
+        data={data.body.products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.itemContainer}>
+            <Image
+              source={{ uri: item.coverImageUrl }}
+              style={styles.itemImage}
+            />
+            <Text style={styles.itemName}>{item.name}</Text>
+            <Text style={styles.itemPrice}>${item.price}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -46,7 +58,7 @@ const styles = StyleSheet.create({
   },
   header: {
     width: "100%",
-    height: 350,
+    height: 470,
     flexShrink: 0,
     backgroundColor: "#00FF00",
   },
@@ -82,5 +94,57 @@ const styles = StyleSheet.create({
   buttonImage: {
     width: 30,
     height: 30,
+  },
+  itemContainer: {
+    flex: 1,
+    height: 279,
+    flexShrink: 0,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    marginBottom: 20,
+    backgroundColor: "#FFFFFF",
+    ...Platform.select({
+      ios: {
+        shadowColor: "#000000",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
+  },
+  itemImage: {
+    width: "100%",
+    height: 209,
+    flexShrink: 0,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+  },
+  itemName: {
+    width: 117,
+    height: 26,
+    left: 18,
+    marginTop: 10,
+    flexShrink: 0,
+    fontFamily: "Poppins Medium",
+    fontWeight: "600",
+    fontSize: 16,
+    fontStyle: "normal",
+    lineHeight: 24,
+    color: "#000000",
+  },
+  itemPrice: {
+    width: 45,
+    height: 25,
+    left: 18,
+    flexShrink: 0,
+    fontFamily: "Poppins Regular",
+    fontWeight: "400",
+    fontSize: 14,
+    fontStyle: "normal",
+    lineHeight: 24,
+    color: "#000000",
   },
 });
