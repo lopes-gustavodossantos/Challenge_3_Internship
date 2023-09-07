4b09ed35878f1ab4bd5b8b9d8ca02b3423c0ae3f
Gustavo dos Santos Lopes <lopes.gustavodossantos@gmail.com>
2023-09-07 19:08:39 -0300

    feat: created Most Popular Card system in Home Screen

--- ChallengeIII/app/(tabs)/home.tsx
+++ ChallengeIII/app/(tabs)/home.tsx
@@ -16,6 +16,8 @@ export default function HomeScreen() {
     return null;
   }
 
+  const popularItems = data.body.products.filter((item) => item.popular === 1);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -31,11 +33,37 @@ export default function HomeScreen() {
             style={styles.buttonImage}
           />
         </Pressable>
+
+        <FlatList
+          data={popularItems}
+          keyExtractor={(item) => item.id.toString()}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          renderItem={({ item, index }) => (
+            <View
+              style={[
+                styles.popularItemContainer,
+                index === 0 ? { marginLeft: 24 } : {},
+                index === popularItems.length - 1 ? { marginRight: 24 } : {},
+              ]}
+            >
+              <Image
+                source={{ uri: item.coverImageUrl }}
+                style={styles.popularItemImage}
+              />
+              <View style={styles.popularItemTextContainer}>
+                <Text style={styles.popularItemName}>{item.name}</Text>
+                <Text style={styles.popularItemPrice}>${item.price}</Text>
+              </View>
+            </View>
+          )}
+        />
       </View>
 
       <FlatList
         data={data.body.products}
         keyExtractor={(item) => item.id.toString()}
+        showsVerticalScrollIndicator={false}
         renderItem={({ item }) => (
           <View style={styles.itemContainer}>
             <Image
@@ -58,7 +86,7 @@ const styles = StyleSheet.create({
   },
   header: {
     width: "100%",
-    height: 470,
+    height: 450,
     flexShrink: 0,
     backgroundColor: "#00FF00",
   },
@@ -94,6 +122,59 @@ const styles = StyleSheet.create({
   buttonImage: {
     width: 30,
     height: 30,
+  },
+  popularItemContainer: {
+    flexDirection: "row",
+    width: 287,
+    height: 140,
+    marginTop: 170,
+    marginHorizontal: 5,
+    flexShrink: 0,
+    borderRadius: 8,
+    backgroundColor: "#FFFFFF",
+    ...Platform.select({
+      ios: {
+        shadowColor: "#000000",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+      },
+      android: {
+        elevation: 4,
+      },
+    }),
+  },
+  popularItemImage: {
+    width: 150,
+    height: "100%",
+    borderRadius: 8,
+  },
+  popularItemTextContainer: {
+    flexDirection: 'column',
+    left: 16,
+    flexShrink: 0,
+  },
+  popularItemName: {
+    width: 85,
+    height: 16,
+    marginTop: 8,
+    fontFamily: "Poppins Regular",
+    fontWeight: "500",
+    fontSize: 14,
+    fontStyle: "normal",
+    lineHeight: 16,
+    color: "#000000",
+  },
+  popularItemPrice: {
+    width: 39,
+    height: 14,
+    marginTop: 5,
+    fontFamily: "Poppins Medium",
+    fontWeight: "600",
+    fontSize: 14,
+    fontStyle: "normal",
+    lineHeight: 16,
+    color: "#000000",
   },
   itemContainer: {
     flex: 1,
